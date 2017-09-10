d9a457f54ee1fdf11ab97c6cd7416db0312361ca
Flynn Joffray <nucleardreamer@gmail.com>
2017-09-10 13:04:53 -0700

    more

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,17 +9,19 @@ sudo usermod -aG docker $(whoami)
 sudo mkdir -p \
   $MAIN_DIR/movies \
   $MAIN_DIR/tv \
+  $MAIN_DIR/nginx/config \
+  $MAIN_DIR/nginx/html \
   $MAIN_DIR/unifi/data \
   $MAIN_DIR/unifi/logs \
   $MAIN_DIR/mysql/data \
   $MAIN_DIR/plex/config \
   $MAIN_DIR/plex/transcode \
   $MAIN_DIR/sonarr/config \
+  $MAIN_DIR/jackett/config \
+  $MAIN_DIR/radarr/config \
   $MAIN_DIR/transmission_movies/incomplete \
   $MAIN_DIR/transmission_movies/home \
   $MAIN_DIR/transmission_movies/config \
   $MAIN_DIR/transmission_tv/incomplete \
   $MAIN_DIR/transmission_tv/home \
-  $MAIN_DIR/transmission_tv/config \
-  $MAIN_DIR/syncthing/config \
-  $MAIN_DIR/syncthing/data
+  $MAIN_DIR/transmission_tv/config
